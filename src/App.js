7719c235956f685edfc73c4781b356b22c4df632
Wlad <wladkrychin@gmail.com>
2021-09-16 12:44:14 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
-import { v4 as uuidv4 } from "uuid";
 import "./index.css";
-// import { useState, useEffect } from "react";
 import PhoneList from "./components/PhoneList/PhoneList";
 import ContactForm from "./components/ContactForm/ContactForm";
 import Filter from "./components/Filter/Filter";
 
 function App() {
-  // const [contacts, setContacts] = useState(
-  //   () => JSON.parse(window.localStorage.getItem("contacts")) ?? [],
-  // );
-
-  // useEffect(() => {
-  //   window.localStorage.setItem("contacts", JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const handleContactAdd = ({ name, tel }) => {
-  //   const contact = {
-  //     id: uuidv4(),
-  //     name,
-  //     tel,
-  //   };
-
-  //   if (
-  //     contacts.find(
-  //       (contact) => contact.name.toLowerCase() === name.toLowerCase()
-  //     )
-  //   ) {
-  //     alert(`${name} is already in contacts.`);
-  //   } else if (contacts.find((contact) => contact.tel === tel)) {
-  //     alert(`${tel} is already in contacts.`);
-  //   } else {
-  //     setContacts((prevContacts) => [...prevContacts, contact]);
-  //   }
-  // };;
   return (
     <div className="container">
       <h1>PhoneBooks</h1>
